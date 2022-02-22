8af10b95c8e8e80c2b34c2c404fb8e7e235080f1
ramzy <ramsesfeukeng@gmail.com>
2022-02-22 01:52:25 +0100

    lastCommitForTaskModel

--- src/todoApp/admin.py
+++ src/todoApp/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import *
 
+
+class AdminTask(admin.ModelAdmin):
+    list_display = ('id','title', 'completed', 'created')
 # Register your models here.
-admin.site.register(Task)
+admin.site.register(Task, AdminTask)
