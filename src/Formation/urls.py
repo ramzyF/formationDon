6eaa9a1a88a3854167b4fac10477d929e0cbbaf7
ramzy <ramsesfeukeng@gmail.com>
2022-02-27 04:00:29 +0100

    start of Orange project

--- src/Formation/urls.py
+++ src/Formation/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from products.views import *
+from orangeAgent.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
